6e01890892b7dcf51b055785401f2829d6fd65ef
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2023-04-22 00:05:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
+API_ID       = "3334521"
+API_HASH     = "29edd7420d528140c7a04bd47486886f"
+BOT_TOKEN    = "5609206256:AAHJH6E7P61QcwU86QVXftxpstRkn6xedks"
 SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
+DATABASE_URI = "mongodb+srv://Aryan6969:Aryan6969@cluster0.krhmwhe.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001856581505
+ADMIN        = 5079629749
 CHANNEL      = "@GreyMatter_Bots"
