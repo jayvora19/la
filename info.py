a56e85037b0a3060ea198cafbedd7574d5ac31de
jayvora19 <75425454+jayvora19@users.noreply.github.com>
2023-05-17 11:33:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = "3334521"
-API_HASH     = "29edd7420d528140c7a04bd47486886f"
-BOT_TOKEN    = "5609206256:AAHJH6E7P61QcwU86QVXftxpstRkn6xedks"
-SESSION      = ""
-DATABASE_URI = "mongodb+srv://Aryan6969:Aryan6969@cluster0.krhmwhe.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1001856581505
-ADMIN        = 5079629749
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = "26861990"
+API_HASH     = "0592761ae3a24dcf709d85ab87bc12b9"
+BOT_TOKEN    = "6004178116:AAGXlZx-VWry9DVw2hooFQrwmKkB8tL1uuc"
+SESSION      = "BQGZ4aYAvtsIfMJpRIIBLTKskWOD9-zdV25gexQ-744mAMfM9xDKmtD4LsGdWp4srQtcl-U-n6v241JtTxORuffwdE_E3flodf4ZXt_qI5ELZ8w1upGaYlvnjIjw8De_Vj_idleWlhdXj7o2oTYEuCkU49wx7S_w3GfnVo9Hx-VzjRMKp2zjkErdNa5UsXxKt-xuSsCF6iRcMXvT08jMOYIiidJ3snEX4ZbUYtojEzlzajmnaB8DOkq1waL8DwVOGp6RQvizJOrFtxi_FPbmxtvEoCClKHfHvEP_tsn6B--dtS3V26vwC5XA_pT0TkeCbCRJ4t6Q4_u5VHPYamd7Q90g7qAr1AAAAAFnifBOAA"
+DATABASE_URI = "mongodb+srv://jayvora:1850@cluster.t5ql7o3.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001847058383
+ADMIN        = 6032060494
+CHANNEL      = "@FreeFlix18"
